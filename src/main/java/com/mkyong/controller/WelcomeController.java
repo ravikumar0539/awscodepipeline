072268cf62d5d8bfbc449a9e25fa5d67f5847ec9
ravikumar0539 <kravikumar2295@gmail.com>
2021-05-20 12:45:47 +0530

    Update WelcomeController.java

--- src/main/java/com/mkyong/controller/WelcomeController.java
+++ src/main/java/com/mkyong/controller/WelcomeController.java
@@ -16,7 +16,7 @@ public class WelcomeController {
     @Value("${welcome.message}")
     private String message;
 
-    private List<String> tasks = Arrays.asList("a", "b", "c", "d", "e", "frar", "g");
+    private List<String> tasks = Arrays.asList("a", "b", "c", "d", "e", "frar2", "g");
 
     @GetMapping("/")
     public String main(Model model) {
